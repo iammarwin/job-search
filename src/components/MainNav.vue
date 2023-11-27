05787a952484a4a779b9f345ba62afde02420462
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-11-27 15:53:06 +0100

    change css file name

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="with-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full">
       <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
-        <a href="?" class="flex h-full items-center text-xl">CareerConnect</a>
+        <a href="/" class="flex h-full items-center text-xl">CareerConnect</a>
       </div>
     </div>
   </header>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import '@/index.css'
+import '@/main.css'
 import App from '@/App.vue'
 const app = createApp(App)
 app.mount('#app')
