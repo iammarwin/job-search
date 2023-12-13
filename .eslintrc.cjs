fb5eaa0ce14802a8fc4152a3b7d4d33324f2ddc6
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-13 16:11:49 +0100

    play with vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,13 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vitest-golbals/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    'vitest-globals/env': true
   }
 }
--- tests/unit/playgroung.test.js
+++ tests/unit/playgroung.test.js
@@ -1,4 +1,3 @@
-import { describe, it, expect } from 'vitest'
 import { evenOrOdd, multiply } from '../../src/playground.js'
 describe('basic math', () => {
   it('Adds two numbers', () => {
