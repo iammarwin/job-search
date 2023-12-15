e18a14e07e23a5fbbce2aee45fe8f140d67fcb02
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-15 18:53:40 +0100

    add ActionButton component

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="hover:shadow-bhover shadow-blue rounded border-0 bg-brand-blue-1 px-6 py-2 font-medium text-white transition duration-200 ease-in-out hover:bg-[#1b66c9]"
+  >
+    Sign In
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ActionButton'
+}
+</script>
