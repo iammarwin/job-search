a8d60f30229867e46a011e6920a03ce2bc7c03fc
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-15 18:54:36 +0100

    add ProfileButton component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+    src="@/assets/images/team-1-150x150.jpg"
+    alt="user profile image"
+    class="h-9 w-9 rounded-3xl object-contain"
+  />
+</template>
+<script>
+export default {
+  name: 'ProfileImage'
+}
+</script>
