cd425639b43084b0e607bbb9dfdf9ba46d29ceb3
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-21 23:33:14 +0100

    install font awesome icons and import it to use as a global component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import '@/main.css'
 import App from '@/App.vue'
+
+library.add(faSearch)
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
