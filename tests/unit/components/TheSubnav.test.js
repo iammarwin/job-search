eaea29d3693804af25ee029ead86ca746984d858
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-21 23:26:52 +0100

    test TheSubnav component

---
+++ tests/unit/components/TheSubnav.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/vue'
+import TheSubnav from '@/components/TheSubnav.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+describe('TheSubnav', () => {
+  describe('when user is on jobs page', () => {
+    it('displays job count', () => {
+      render(TheSubnav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true
+          }
+        },
+        data() {
+          return {
+            onJobResultsPage: true
+          }
+        }
+      })
+      const jobCount = screen.getByText('1426')
+      expect(jobCount).toBeInTheDocument()
+    })
+  })
+
+  describe('when user is not on jobs page', () => {
+    it('does not display job count', () => {
+      render(TheSubnav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true
+          }
+        },
+        data() {
+          return {
+            onJobResultsPage: false
+          }
+        }
+      })
+      const jobCount = screen.queryByText('1426')
+      expect(jobCount).not.toBeInTheDocument()
+    })
+  })
+})
