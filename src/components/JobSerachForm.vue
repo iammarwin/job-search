e1de9a408a5c0382a9a1bf03af7fd09a6ff02b44
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-26 23:13:08 +0100

    create JobSearchForm component

---
+++ src/components/JobSerachForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mt-16">
+    <form class="flex h-12 w-full items-center gap-4">
+      <fieldset>
+        <label for="role">Role</label>
+        <div>
+          <font-awesome-icon v-bind:icon="['fas', 'search']" />
+          <input type="text" id="role" placeholder="Software Engineer" />
+        </div>
+      </fieldset>
+      <fieldset>
+        <label for="location">Where?</label>
+        <div>
+          <font-awesome-icon :icon="['fas', 'location-dot']" />
+          <input type="text" id="location" placeholder="Los Angeles" />
+        </div>
+      </fieldset>
+      <action-button btnText="Search" btnType="secondary" />
+    </form>
+  </div>
+</template>
+
+<script>
+import ActionButton from '@/components/ActionButton.vue'
+export default {
+  name: 'JobSearchForm',
+  components: {
+    ActionButton
+  }
+}
+</script>
