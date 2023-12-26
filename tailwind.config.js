e05fe7a12bfdbae2d69baad238cec37e35562fa4
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-26 18:07:33 +0100

    Add New Colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       },
       colors: {
         'brand-gray-1': '#dadce0',
+        'brand-gray-2': '#f8f9fa',
+        'brand-gray-3': '#80868b',
         'brand-blue-1': '#1a73e8',
         'brand-blue-2': '#4285f4',
         'brand-blue-3': '#1b66c9',
