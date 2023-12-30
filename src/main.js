2c9bf26ec9ec6a1784302bbfbe9f4eb1c60af498
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-30 22:46:12 +0100

    install vue-router and import it to use

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 import '@/main.css'
 import App from '@/App.vue'
+import router from '@/router'
 
 library.add(faSearch, faLocationDot)
+
 const app = createApp(App)
+app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
