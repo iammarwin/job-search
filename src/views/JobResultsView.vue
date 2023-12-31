91663235e54fa4404c9d9a39da4a8d0a4d8d3855
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-31 23:32:08 +0100

    add JobView component

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1 class="text-6xl font-bold">Job Results page</h1>
+  <div class="relative flex w-full flex-row flex-nowrap items-start">
+    <job-filters-sidebar />
+    <job-listings />
+  </div>
 </template>
 
 <script>
+import JobFiltersSidebar from '@/components/JobResults/JobFiltersSidebar.vue'
+import JobListings from '@/components/JobResults/JobListings.vue'
 export default {
-  name: 'JobSearchView'
+  name: 'JobSearchView',
+  components: {
+    JobFiltersSidebar,
+    JobListings
+  }
 }
 </script>
