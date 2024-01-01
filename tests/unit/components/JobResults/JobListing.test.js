dd1aa872b89238ad4627349e28d533bd9e36d20c
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-01 19:42:53 +0100

    test JobListing component

---
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/vue'
+import { RouterLinkStub } from '@vue/test-utils'
+
+import JobListing from '@/components/JobResults/JobListing.vue'
+
+describe('JobListing', () => {
+  const createJobProps = (props) => ({
+    title: 'Vue Programmer',
+    organization: 'Apple',
+    ...props
+  })
+  const renderJobListing = (Jobprops) => {
+    render(JobListing, {
+      global: {
+        stubs: {
+          'router-link': RouterLinkStub
+        }
+      },
+      props: {
+        job: {
+          ...Jobprops
+        }
+      }
+    })
+  }
+  it('renders job title', () => {
+    const jobProps = createJobProps({ title: 'Vue Developer' })
+    renderJobListing(jobProps)
+    expect(screen.getByText('Vue Developer')).toBeInTheDocument()
+  })
+  it('renders job organization', () => {
+    const jobProps = createJobProps({ organization: 'ArkConnect' })
+    renderJobListing(jobProps)
+    expect(screen.getByText('ArkConnect')).toBeInTheDocument()
+  })
+
+  it('renders job location', () => {
+    const jobProps = createJobProps({ locations: ['Casablaca', 'Marrakesh'] })
+    renderJobListing(jobProps)
+    expect(screen.getByText('Casablaca')).toBeInTheDocument()
+    expect(screen.getByText('Marrakesh')).toBeInTheDocument()
+  })
+  it('renders job qualification', () => {
+    const jobProps = createJobProps({ minimumQualifications: ['Code', 'Develop'] })
+    renderJobListing(jobProps)
+    expect(screen.getByText('Code')).toBeInTheDocument()
+    expect(screen.getByText('Develop')).toBeInTheDocument()
+  })
+})
