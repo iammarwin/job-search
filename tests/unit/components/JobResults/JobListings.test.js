353e14061661301072f20ce09f0664f785695c56
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-02 16:30:02 +0100

    Fixing Failing JobListings Component Tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -10,14 +10,25 @@ vi.mock('axios')
 describe('JobListings', () => {
   it('fetches jobs', () => {
     axios.get.mockResolvedValue({ data: [] })
-    render(JobListings)
+    const $route = { query: { page: 5 } }
+    render(JobListings, {
+      global: {
+        mocks: {
+          $route
+        }
+      }
+    })
     expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs/')
   })
 
-  it('creates a job listing for every job', async () => {
+  it('displays maximum of 10 jobs', async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    const $route = { query: { page: 1 } }
     render(JobListings, {
       global: {
+        mocks: {
+          $route
+        },
         stubs: {
           RouterLink: RouterLinkStub
         }
@@ -25,6 +36,6 @@ describe('JobListings', () => {
     })
 
     const jobListings = await screen.findAllByRole('listitem')
-    expect(jobListings).toHaveLength(15)
+    expect(jobListings).toHaveLength(10)
   })
 })
