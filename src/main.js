635069f531ddb8bc46effcf3130a57a6b69a3c2c
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-03 18:45:04 +0100

    install and register pinia store

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faLocationDot } from '@fortawesome/free-solid-svg-icons'
@@ -8,8 +9,10 @@ import App from '@/App.vue'
 import router from '@/router'
 
 library.add(faSearch, faLocationDot)
+const pinia = createPinia()
 
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
