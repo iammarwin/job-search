7aa6ac00f1a5f4490e1103168c1f58674ce122c8
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-05 16:22:22 +0100

    Update Font Awesome Icons

--- src/components/JobResults/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="flex w-96 flex-col border-r border-brand-gray-1 bg-white p-4">JobFiltersSidebar</div>
+  <div class="flex w-96 flex-col border-r border-brand-gray-1 bg-white p-4">
+    <section class="pb-5">
+      <div class="flex flex-row items-center justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want do do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button btnText="Clear Filters" btnType="tertiary" />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import ActionButton from '@/components/Shared/ActionButton.vue'
 export default {
-  name: 'JobFiltersSidebar'
+  name: 'JobFiltersSidebar',
+  components: {
+    ActionButton
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faLocationDot, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
 import '@/main.css'
 import App from '@/App.vue'
 import router from '@/router'
 
-library.add(faSearch, faLocationDot)
+library.add(faSearch, faLocationDot, faAngleDown, faAngleUp)
 const pinia = createPinia()
 
 const app = createApp(App)
