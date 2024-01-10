1e3ea89b68f5bd659a272dc1aa9f03ffb8455d14
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-10 15:31:11 +0100

    TDD: Adding Getter to Filter Jobs by Organization

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,8 +1,12 @@
 import { defineStore } from 'pinia'
+
 import getJobs from '@/api/getJobs'
+
+import { useUserStore } from '@/stores/user.js'
 
 export const FETCH_JOBS = 'FETCH_JOBS'
 export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
+export const FILTERED_JOBS_BY_ORGANIZATIONS = 'FILTERED_JOBS_BY_ORGANIZATIONS'
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
     jobs: []
@@ -14,6 +18,10 @@ export const useJobsStore = defineStore('jobs', {
         uniqueOrganization.add(job.organization)
       })
       return uniqueOrganization
+    },
+    [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
+      const userStore = useUserStore()
+      return state.jobs.filter((job) => userStore.selectedOrganizations.includes(job.organization))
     }
   },
   actions: {
--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -1,7 +1,9 @@
 import { createPinia, setActivePinia } from 'pinia'
 import axios from 'axios'
 
-import { useJobsStore } from '@/stores/jobs'
+import { useJobsStore } from '@/stores/jobs.js'
+import { useUserStore } from '@/stores/user.js'
+import { describe } from 'vitest'
 
 vi.mock('axios')
 
@@ -47,6 +49,28 @@ describe('getters', () => {
 
       const result = store.UNIQUE_ORGANIZATIONS
       expect(result).toEqual(new Set(['Google', 'Amazon']))
+    })
+  })
+
+  describe('FILTERED_JOBS_BY_ORGANIZATIONS', () => {
+    it('identifies jobs that are associated with the given organizations', () => {
+      const jobsStore = useJobsStore()
+      jobsStore.jobs = [
+        { organization: 'Google' },
+        { organization: 'Amazon' },
+        { organization: 'Microsoft' }
+      ]
+      const userStore = useUserStore()
+      userStore.selectedOrganizations = ['Google', 'Microsoft']
+
+      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS
+
+      expect(result).toEqual([
+        {
+          organization: 'Google'
+        },
+        { organization: 'Microsoft' }
+      ])
     })
   })
 })
