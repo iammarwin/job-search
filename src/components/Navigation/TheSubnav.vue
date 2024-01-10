bd8bb84454beb3a962c86f714487f7c1a77cf28e
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-10 23:21:52 +0100

    Displaying Accurate Number of Filtered Jobs

--- src/components/Navigation/TheSubnav.vue
+++ src/components/Navigation/TheSubnav.vue
@@ -1,23 +1,30 @@
+<template>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">
+      <div v-if="onJobResultsPage" class="flex items-center gap-3">
+        <font-awesome-icon :icon="['fas', 'search']" />
+        <p class="text-lg">
+          <span class="font-semibold text-brand-green-1">
+            {{ FILTERED_JOBS_BY_ORGANIZATIONS.length }}
+          </span>
+          jobs matched
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
+import { mapState } from 'pinia'
+import { useJobsStore, FILTERED_JOBS_BY_ORGANIZATIONS } from '@/stores/jobs.js'
+
 export default {
   name: 'TheSubnav',
   computed: {
+    ...mapState(useJobsStore, [FILTERED_JOBS_BY_ORGANIZATIONS]),
     onJobResultsPage() {
       return this.$route.name === 'JobResults'
     }
   }
 }
 </script>
-
-<template>
-  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
-    <div class="flex h-full items-center px-8">
-      <div class="flex items-center gap-3" v-if="onJobResultsPage">
-        <font-awesome-icon v-bind:icon="['fas', 'search']" />
-        <p class="text-lg">
-          <span class="font-semibold text-brand-green-1">1426</span> jobs matched
-        </p>
-      </div>
-    </div>
-  </div>
-</template>
--- tests/unit/components/Navigation/TheSubnav.test.js
+++ tests/unit/components/Navigation/TheSubnav.test.js
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/vue'
+import { createTestingPinia } from '@pinia/testing'
+
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { useJobsStore } from '@/stores/jobs.js'
 
 describe('TheSubnav', () => {
   const renderTheSubNav = (routeName) => {
+    const pinia = createTestingPinia()
+    const jobsStore = useJobsStore()
     render(TheSubnav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route: {
             name: routeName
@@ -16,12 +21,17 @@ describe('TheSubnav', () => {
         }
       }
     })
+    return { jobsStore }
   }
   describe('when user is on jobs page', () => {
-    it('displays job count', () => {
+    it('displays job count', async () => {
       const routeName = 'JobResults'
-      renderTheSubNav(routeName)
-      const jobCount = screen.getByText('1426')
+
+      const { jobsStore } = renderTheSubNav(routeName)
+      const numberOfJobs = 16
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({})
+
+      const jobCount = await screen.findByText(numberOfJobs)
       expect(jobCount).toBeInTheDocument()
     })
   })
@@ -29,8 +39,12 @@ describe('TheSubnav', () => {
   describe('when user is not on jobs page', () => {
     it('does not display job count', () => {
       const routeName = 'Home'
-      renderTheSubNav(routeName)
-      const jobCount = screen.queryByText('1426')
+
+      const { jobsStore } = renderTheSubNav(routeName)
+      const numberOfJobs = 16
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({})
+
+      const jobCount = screen.queryByText(numberOfJobs)
       expect(jobCount).not.toBeInTheDocument()
     })
   })
