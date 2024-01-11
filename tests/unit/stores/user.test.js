7b7d609018f2910d0515d1ef9c45d416934a6c78
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-11 13:03:32 +0100

    TDD: Adding State for Job Types & Adding Action for Selecting Job Types

--- tests/unit/stores/user.test.js
+++ tests/unit/stores/user.test.js
@@ -9,13 +9,18 @@ describe('state', () => {
   })
 
   it('keeps track if user is logged in', () => {
-    const store = useUserStore()
-    expect(store.isLoggedIn).toBe(false)
+    const userStore = useUserStore()
+    expect(userStore.isLoggedIn).toBe(false)
   })
 
   it('stores organizations that the user would like to filter jobs by', () => {
-    const store = useUserStore()
-    expect(store.selectedOrganizations).toEqual([])
+    const userStore = useUserStore()
+    expect(userStore.selectedOrganizations).toEqual([])
+  })
+
+  it('stores jobs type that the user would like to filter jobs by', () => {
+    const userStore = useUserStore()
+    expect(userStore.selectedJobTypes).toEqual([])
   })
 })
 
@@ -26,17 +31,25 @@ describe('actions', () => {
 
   describe('loginUser', () => {
     it('logs the user in', () => {
-      const store = useUserStore()
-      store.loginUser()
-      expect(store.isLoggedIn).toBe(true)
+      const userStore = useUserStore()
+      userStore.loginUser()
+      expect(userStore.isLoggedIn).toBe(true)
     })
   })
 
   describe('ADD_SELECTED_ORGANIZATIONS', () => {
     it('updates organizations the user has shosen to filer jobs by', () => {
-      const store = useUserStore()
-      store.ADD_SELECTED_ORGANIZATIONS(['Org1', 'Org2'])
-      expect(store.selectedOrganizations).toEqual(['Org1', 'Org2'])
+      const userStore = useUserStore()
+      userStore.ADD_SELECTED_ORGANIZATIONS(['Org1', 'Org2'])
+      expect(userStore.selectedOrganizations).toEqual(['Org1', 'Org2'])
+    })
+  })
+
+  describe('ADD_SELECTED_JOB_TYPES', () => {
+    it('updates job types the user has chosen to filter jobs by', () => {
+      const userStore = useUserStore()
+      userStore.ADD_SELECTED_JOB_TYPES(['Full-time', 'Part-time'])
+      expect(userStore.selectedJobTypes).toEqual(['Full-time', 'Part-time'])
     })
   })
 })
