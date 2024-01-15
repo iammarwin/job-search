63c5566174bfdec9ec4a0ad1801b8d88944b9379
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-15 18:43:14 +0100

    Composition API with Vue Router

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -9,8 +9,8 @@
         <div
           class="flex w-full items-center gap-x-2 rounded-full border border-brand-gray-3 px-4 py-3"
         >
-          <font-awesome-icon class="text-brand-gray-3" v-bind:icon="['fas', 'search']" />
-          <text-input placeholder="Software Engineer" id="role" v-model="role" />
+          <font-awesome-icon :icon="['fas', 'search']" class="text-brand-gray-3" />
+          <text-input id="role" v-model="role" placeholder="Software Engineer" />
         </div>
       </fieldset>
       <fieldset class="flex w-full flex-col gap-y-2 lg:flex-1">
@@ -19,35 +19,27 @@
           class="flex w-full items-center gap-x-2 rounded-full border border-brand-gray-3 px-4 py-3"
         >
           <font-awesome-icon class="text-brand-gray-3" :icon="['fas', 'location-dot']" />
-          <text-input placeholder="Los Angeles" id="location" v-model="location" />
+          <text-input id="location" v-model="location" placeholder="Los Angeles" />
         </div>
       </fieldset>
       <div class="lg:self-end">
-        <action-button btnText="Search" btnType="secondary" class="w-full" />
+        <action-button btn-text="Search" btn-type="secondary" class="w-full" />
       </div>
     </form>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import TextInput from '@/components/Shared/TextInput.vue'
-export default {
-  name: 'JobSearchForm',
-  components: {
-    ActionButton,
-    TextInput
-  },
-  data() {
-    return {
-      role: '',
-      location: ''
-    }
-  },
-  methods: {
-    searchForJobs() {
-      this.$router.push({ name: 'JobResults', query: { role: this.role, location: this.location } })
-    }
-  }
+
+const router = useRouter()
+const role = ref('')
+const location = ref('')
+
+const searchForJobs = () => {
+  router.push({ name: 'JobResults', query: { role: role.value, location: location.value } })
 }
 </script>
