85e1579f7a7a02050121e6897cfa9c21d6658e34
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-17 00:22:08 +0100

    composition api with route and pinia getters

--- src/components/Navigation/TheSubnav.vue
+++ src/components/Navigation/TheSubnav.vue
@@ -14,17 +14,15 @@
   </div>
 </template>
 
-<script>
-import { mapState } from 'pinia'
-import { useJobsStore, FILTERED_JOBS } from '@/stores/jobs.js'
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
-export default {
-  name: 'TheSubnav',
-  computed: {
-    ...mapState(useJobsStore, [FILTERED_JOBS]),
-    onJobResultsPage() {
-      return this.$route.name === 'JobResults'
-    }
-  }
-}
+import { useJobsStore } from '@/stores/jobs.js'
+
+const route = useRoute()
+const onJobResultsPage = computed(() => route.name === 'JobResults')
+
+const jobStore = useJobsStore()
+const FILTERED_JOBS = computed(() => jobStore.FILTERED_JOBS)
 </script>
