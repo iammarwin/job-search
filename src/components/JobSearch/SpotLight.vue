2532a1f7efeaa3ac4d083b97556e4e6f4803d74c
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-18 15:51:06 +0100

    The onMounted Function

--- src/components/JobSearch/SpotLight.vue
+++ src/components/JobSearch/SpotLight.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="spotlight in spotLights" :key="spotlight.id" class="h-full">
+    <li v-for="spotlight in spotlights" :key="spotlight.id" class="h-full">
       <slot
         :img="spotlight.img"
         :title="spotlight.title"
@@ -10,22 +10,18 @@
   </ul>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-axios
-export default {
-  name: 'SpotLight',
-  data() {
-    return {
-      spotLights: []
-    }
-  },
-  async mounted() {
-    const BASE_URL = import.meta.env.VITE_APP_API_URL
-    const URL = `${BASE_URL}/spotlights`
-    const response = await axios.get(URL)
-    this.spotLights = response.data
-  }
+const spotlights = ref([])
+
+const getSpotlights = async () => {
+  const BASE_URL = import.meta.env.VITE_APP_API_URL
+  const URL = `${BASE_URL}/spotlights`
+  const response = await axios.get(URL)
+  spotlights.value = response.data
 }
+
+onMounted(getSpotlights)
 </script>
