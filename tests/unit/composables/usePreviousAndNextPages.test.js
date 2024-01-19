48ed31e99fe01ae67ebe6f54d94460b6dd0d4410
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-19 10:08:44 +0100

    Adding Tests for usePreviousAndNextPages Composable

---
+++ tests/unit/composables/usePreviousAndNextPages.test.js
@@ -0,0 +1,37 @@
+import usePreviousAndNextPages from '@/composables/usePreviousAndNextPages.js'
+
+import { describe, expect } from 'vitest'
+
+describe('usePreviousAndNextPages', () => {
+  it('calculates page before current one', () => {
+    const currentPage = { value: 8 }
+    const lastPage = { value: 10 }
+    const { previousPage } = usePreviousAndNextPages(currentPage, lastPage)
+    expect(previousPage.value).toBe(7)
+  })
+
+  describe('when current page is the firt page', () => {
+    it('does not provide previous page', () => {
+      const currentPage = { value: 1 }
+      const lastPage = { value: 10 }
+      const { previousPage } = usePreviousAndNextPages(currentPage, lastPage)
+      expect(previousPage.value).toBeUndefined()
+    })
+  })
+
+  it('calculates page after current one', () => {
+    const currentPage = { value: 8 }
+    const lastPage = { value: 10 }
+    const { nextPage } = usePreviousAndNextPages(currentPage, lastPage)
+    expect(nextPage.value).toBe(9)
+  })
+
+  describe('when current page is the last page', () => {
+    it('does not provide next page', () => {
+      const currentPage = { value: 10 }
+      const lastPage = { value: 10 }
+      const { nextPage } = usePreviousAndNextPages(currentPage, lastPage)
+      expect(nextPage.value).toBeUndefined()
+    })
+  })
+})
