9af1614280116764dc58ab424fc7d90e0e813e2c
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-22 12:13:46 +0100

    create job interface and update getJobs API call

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
+
+import type { Job } from '@/api/types'
 const getJobs = async () => {
   const BASE_URL = import.meta.env.VITE_APP_API_URL
   const URL = `${BASE_URL}/jobs`
-  const response = await axios.get(URL)
+  const response = await axios.get<Job[]>(URL)
   return response.data
 }
 
