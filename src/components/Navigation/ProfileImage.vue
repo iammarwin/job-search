a8aa9deef6307985f9bed6da4e2b465a81302a25
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-22 19:03:29 +0100

    Adding Type Annotations to ProfileImage Component

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -1,12 +1,11 @@
 <template>
-  <img
-    src="@/assets/images/team-1-150x150.jpg"
-    alt="user profile image"
-    class="h-9 w-9 rounded-3xl object-contain"
-  />
+  <img :src="imageLink" alt="user profile image" class="h-9 w-9 rounded-3xl object-contain" />
 </template>
-<script>
-export default {
-  name: 'ProfileImage'
-}
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const imageLink = ref(
+  'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
 </script>
