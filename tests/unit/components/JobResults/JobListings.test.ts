6286edaa30e93d17df564eb6819cd68a3a4dd84a
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-24 01:01:12 +0100

    Adding Type Annotations to JobListings Component

--- tests/unit/components/JobResults/JobListings.test.ts
+++ tests/unit/components/JobResults/JobListings.test.ts
@@ -1,8 +1,11 @@
+import type { Mock } from 'vitest'
 import { render, screen } from '@testing-library/vue'
 import { RouterLinkStub } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import { useRoute } from 'vue-router'
+
 vi.mock('vue-router')
+const useRouteMock = useRoute as Mock
 
 import JobListings from '@/components/JobResults/JobListings.vue'
 import { useJobsStore } from '@/stores/jobs'
@@ -11,11 +14,12 @@ describe('JobListings', () => {
   const renderJobListings = () => {
     const pinia = createTestingPinia()
     const jobsStore = useJobsStore()
+    // @ts-expect-error
     jobsStore.FILTERED_JOBS = Array(15).fill({})
 
     render(JobListings, {
-      plugins: [pinia],
       global: {
+        plugins: [pinia],
         stubs: {
           RouterLink: RouterLinkStub
         }
@@ -25,14 +29,15 @@ describe('JobListings', () => {
   }
 
   it('fetches jobs', () => {
-    useRoute.mockReturnValue({ query: {} })
+    useRouteMock.mockReturnValue({ query: {} })
     const { jobsStore } = renderJobListings()
     expect(jobsStore.FETCH_JOBS).toHaveBeenCalled()
   })
 
   it('displays maximum of 10 jobs', async () => {
-    useRoute.mockReturnValue({ query: { page: 1 } })
+    useRouteMock.mockReturnValue({ query: { page: 1 } })
     const { jobsStore } = renderJobListings()
+    // @ts-expect-error
     jobsStore.FILTERED_JOBS = Array(15).fill({})
 
     const jobListings = await screen.findAllByRole('listitem')
@@ -41,14 +46,14 @@ describe('JobListings', () => {
 
   describe('when params exclude page', () => {
     it('displays page number 1', () => {
-      useRoute.mockReturnValue({ query: {} })
+      useRouteMock.mockReturnValue({ query: {} })
       renderJobListings()
       expect(screen.getByText('Page 1')).toBeInTheDocument()
     })
   })
   describe('when params include page', () => {
     it('displays page number', () => {
-      useRoute.mockReturnValue({ query: { page: '3' } })
+      useRouteMock.mockReturnValue({ query: { page: '3' } })
       renderJobListings()
       expect(screen.getByText('Page 3')).toBeInTheDocument()
     })
@@ -56,7 +61,7 @@ describe('JobListings', () => {
 
   describe('when user is on first page', () => {
     it('does not show link to previous page', async () => {
-      useRoute.mockReturnValue({ query: { page: 1 } })
+      useRouteMock.mockReturnValue({ query: { page: 1 } })
 
       renderJobListings()
       const jobStore = useJobsStore()
@@ -68,9 +73,10 @@ describe('JobListings', () => {
     })
 
     it('shows link to next page', async () => {
-      useRoute.mockReturnValue({ query: { page: 1 } })
+      useRouteMock.mockReturnValue({ query: { page: 1 } })
 
       const { jobsStore } = renderJobListings()
+      // @ts-expect-error
       jobsStore.FILTERED_JOBS = Array(15).fill({})
 
       await screen.findAllByRole('listitem')
@@ -80,8 +86,9 @@ describe('JobListings', () => {
   })
   describe('when user is on last page', () => {
     it('does not show link to next page', async () => {
-      useRoute.mockReturnValue({ query: { page: 2 } })
+      useRouteMock.mockReturnValue({ query: { page: 2 } })
       const { jobsStore } = renderJobListings()
+      // @ts-expect-error
       jobsStore.FILTERED_JOBS = Array(15).fill({})
 
       await screen.findAllByRole('listitem')
@@ -90,9 +97,10 @@ describe('JobListings', () => {
     })
 
     it('shows link to previuous page', async () => {
-      useRoute.mockReturnValue({ query: { page: 2 } })
+      useRouteMock.mockReturnValue({ query: { page: 2 } })
 
       const { jobsStore } = renderJobListings()
+      // @ts-expect-error
       jobsStore.FILTERED_JOBS = Array(15).fill({})
 
       await screen.findAllByRole('listitem')
