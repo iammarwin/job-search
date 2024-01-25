cf6cbf62e09525f0bba8140ec8a2d637b474ba8b
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-01-25 20:01:21 +0100

    TDD for Action: FETCH_DEGREES

--- tests/unit/stores/degrees.test.ts
+++ tests/unit/stores/degrees.test.ts
@@ -1,4 +1,9 @@
+import type { Mock } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
+
+import axios from 'axios'
+vi.mock('axios')
+const axiosGetMock = axios.get as Mock
 
 import { useDegreesStore } from '@/stores/degrees'
 
@@ -10,5 +15,32 @@ describe('state', () => {
   it('stores all degrees that may jobs require', () => {
     const store = useDegreesStore()
     expect(store.degrees).toEqual([])
+  })
+})
+
+describe('actions', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  describe('FETCH_DEGREES', () => {
+    it('makes API request and stores recieved degrees', async () => {
+      axiosGetMock.mockResolvedValue({
+        data: [
+          {
+            id: 1,
+            degree: "Bachelor's"
+          }
+        ]
+      })
+      const store = useDegreesStore()
+      await store.FETCH_DEGREES()
+      expect(store.degrees).toEqual([
+        {
+          id: 1,
+          degree: "Bachelor's"
+        }
+      ])
+    })
   })
 })
